974c735e15453b12217a696f1cb8c2af60fcae0e
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-04-23 21:26:55 +0200

    sortowanie binarne

--- src/com/company/dzien19/ArraysSorting.java
+++ src/com/company/dzien19/ArraysSorting.java
@@ -6,6 +6,13 @@ public class ArraysSorting {
 
     public static void main(String[] args) {
         int[] array = {24, 11, 1, 54, 1, 2, 2, 4, 8, 3, 7};
+        Arrays.sort(array);
+        System.out.println("sorted: " + Arrays.toString(array));
+
+        System.out.println("BS 1: " + Arrays.binarySearch(array, 54));
+
+
+       /* System.out.println(array.length);
 
         System.out.println("Before sorting");
 
@@ -13,6 +20,6 @@ public class ArraysSorting {
         Arrays.sort(array);
 
         System.out.println("After sorting");
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));*/
     }
 }
