13c26205c68a2181482f9b98c620b91a6a1187ad
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-04-23 22:09:30 +0200

    sortowanie bombelkowe

---
+++ src/com/company/dzien19/ArraysSorting1.java
@@ -0,0 +1,35 @@
+package com.company.dzien19;
+
+import java.util.Arrays;
+
+public class ArraysSorting1 {
+    public static void main(String[] args) {
+        int[] array = {24, 11, 1, 54, 2, 6};
+        System.out.println("Non sorted array" + Arrays.toString(array));
+        bubbleSort(array);
+        System.out.println("Sorted array: " +  Arrays.toString(array));
+
+    }
+
+    public static void bubbleSort(int[] array) {
+
+        int size = array.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 1; j < (size - i); j++) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                    System.out.println("Swap " + Arrays.toString(array));
+                } else {
+                    System.out.println("Don't Swap " + array[j - 1] + "and" + array[j]);
+                }
+
+            }
+            System.out.println(i + " i itertion" + Arrays.toString(array));
+            System.out.println();
+                }
+
+            }
+
+        }
