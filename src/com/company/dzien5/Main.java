1ebe82db6705af2bd8081a5ac439f3fa8450ec3f
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-04-24 00:26:22 +0200

    ver 1

---
+++ src/com/company/dzien5/Main.java
@@ -0,0 +1,13 @@
+package com.company.dzien5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+
+        //hello
+
+
+    }
+}
---
+++ src/com/company/dzien5/Main3.java
@@ -0,0 +1,25 @@
+package com.company.dzien5;
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("hello 3");
+
+
+//      ctrl + / = Komentrz jednolinijkowy
+        /*komentarz dluzszy = ctrl + shift + /
+
+
+
+
+       /*
+          komentarz dluższy
+
+
+         */
+
+
+
+
+
+    }
+}
