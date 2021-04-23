b4e057bc60451afe54e75a146b9b2435385210a1
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-04-23 21:02:26 +0200

    sortowanie

--- src/com/company/dzien19/ArraysSorting.java
+++ src/com/company/dzien19/ArraysSorting.java
@@ -6,7 +6,13 @@ public class ArraysSorting {
 
     public static void main(String[] args) {
         int[] array = {24, 11, 1, 54, 1, 2, 2, 4, 8, 3, 7};
-        System.out.println(Arrays.toString(array));
 
+        System.out.println("Before sorting");
+
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array);
+
+        System.out.println("After sorting");
+        System.out.println(Arrays.toString(array));
     }
 }
