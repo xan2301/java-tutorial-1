5da467f631ba5d5c0336c1fac65a94d835c57a95
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-04-26 22:57:10 +0200

    Dodano dzien22 programowanie obiektowe

---
+++ src/com/company/dzien22/Class.java
@@ -0,0 +1,35 @@
+package com.company.dzien22;
+
+public class Class {
+    //    modifier
+    //    class keyword
+    //    class name
+    //    brackets
+    //    body
+
+    private String color;
+
+    //  constructor - used for creating an object
+    //  field (attribute) = variable providing the state of the object
+    //  method
+
+
+    //  method
+    //  modifier, returnType, name, arguments, brackets, body
+
+
+    public void  concatenate(String argument1, String argument2) {
+        if (argument1.equals("stopHere")){
+        return;
+    }
+        System.out.println(argument1 + argument2);
+    //  calling a method
+    //  method can be of type void
+
+    //  object - instance of a class, basic unit of OOP, instance of a class
+    //  object interacts with each other by methods and have state by fields
+    //  object has: state, behaviour, identity (uniqueness)
+
+    //  what is object oriented?
+
+}}
