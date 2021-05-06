4d230178e6f71399dd41bc76a72f0485919f6ef3
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-05-07 00:15:48 +0200

    dzien23 dodano OverLoading

---
+++ src/com/company/dzien23/MethodsOverloading.java
@@ -0,0 +1,16 @@
+package com.company.dzien23;
+
+public class MethodsOverloading {
+
+    private void overloadedMethod (String arg1, String arg2, String arg3){
+
+        System.out.println( "String arg1, String arg2, String arg3" + arg1 + arg2 + arg3);
+    }
+
+    private void overloadedMethod (String arg1, String arg2){
+
+        System.out.println( "String arg1, String arg2, String arg3" + arg1 + arg2);
+    }
+
+
+}
