7132680d55e34f11b750dd706fa49ec0c8be82c3
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-05-14 21:18:58 +0200

    Add day33

---
+++ src/com/company/dzien33/garbage/Cat.java
@@ -0,0 +1,17 @@
+package com.company.dzien33.garbage;
+
+public class Cat {
+
+    public String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/com/company/dzien33/garbage/Example.java
@@ -0,0 +1,11 @@
+package com.company.dzien33.garbage;
+
+public class Example {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Robert");
+        System.gc();
+
+    }
+
+}
