ee164b94fe25467a6b894f84ae33e9f54799e055
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2022-05-27 14:25:19 +0200

    day 4 arrayList

--- src/com/company/dzien43/ListExamples.java
+++ src/com/company/dzien43/ListExamples.java
@@ -6,16 +6,23 @@ import java.util.List;
 public class ListExamples {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-
-            list.add("String" + i);
+        List<String> citiesList = new ArrayList<>();
 
 
-        }
+        citiesList.add("Warsaw");
+        citiesList.add("Gdansk");
+        citiesList.add("Wroclaw");
+        citiesList.add("Szczecin");
 
-        System.out.println(list);
+        System.out.println(citiesList.get(0));
+        System.out.println(citiesList.get(1));
+        System.out.println(citiesList.get(2));
+        System.out.println(citiesList.get(3));
+
+        System.out.println(citiesList.size());
+        System.out.println(citiesList.contains("Szczecin"));
+        System.out.println(citiesList.isEmpty());
+
 
     }
 
---
+++ src/com/company/dzien44/Example1.java
@@ -0,0 +1,14 @@
+package com.company.dzien44;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Example1 {
+
+    List<String> someStringList = new ArrayList<>();
+    Set<String> someSet = new HashSet<>();
+
+
+}
