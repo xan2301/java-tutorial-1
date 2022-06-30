1089022e0596456324fc7b57c74a79848c0e395a
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2022-06-30 18:40:02 +0200

    Project day 1

--- src/com/company/project/model/InputData.java
+++ src/com/company/project/model/InputData.java
@@ -15,11 +15,74 @@ public class InputData {
 
     private RateType rateType = RateType.CONSTANT;
 
-    private BigDecimal bankMargin = new BigDecimal("1.9")
+    private BigDecimal bankMargin = new BigDecimal("1.9");
+
+    public InputData withRepaymentStartDate(LocalDate repaymentStartDate) {
+        this.repaymentStartDate = repaymentStartDate;
+        return this;
+    }
+
+    public InputData withWiborPercent(BigDecimal wiborPercent) {
+        this.wiborPercent = wiborPercent;
+        return this;
+    }
+
+    public InputData withAmount(BigDecimal amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public InputData withMonthsDuration(BigDecimal monthsDuration) {
+        this.monthsDuration = monthsDuration;
+        return this;
+    }
+
+    public InputData withRateType(RateType rateType) {
+        this.rateType = rateType;
+        return this;
+    }
+
+    public InputData withBankMargin(BigDecimal bankMargin) {
+        this.bankMargin = bankMargin;
+        return this;
+    }
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+    public LocalDate getRepaymentStartDate() {
+        return repaymentStartDate;
+    }
+
+    public BigDecimal getWiborPercent() {
+        return wiborPercent;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public BigDecimal getMonthsDuration() {
+        return monthsDuration;
+    }
+
+    public RateType getRateType() {
+        return rateType;
+    }
+
+    public BigDecimal getBankMargin() {
+        return bankMargin;
+    }
 }
