8cbeace8feb3ff1c6365bdc20bae48cde1274ab8
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2022-07-07 18:15:07 +0200

    Project day 5

--- src/com/company/project/service/AmountsCalculationServiceImpl.java
+++ src/com/company/project/service/AmountsCalculationServiceImpl.java
@@ -5,8 +5,11 @@ import com.company.project.model.Rate;
 import com.company.project.model.RateAmounts;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class AmountsCalculationServiceImpl implements AmountsCalculationService {
+
+    public static final BigDecimal YEAR = BigDecimal.valueOf(12);
 
     @Override
     public RateAmounts calculate(InputData inputData) {
@@ -60,7 +63,13 @@ public class AmountsCalculationServiceImpl implements AmountsCalculationService 
     }
 
 
+
+
     private RateAmounts calculateConstantRate(InputData inputData, Rate previousRate) {
+
+        BigDecimal q = calculateQ(inputData.getInterestPercent());
+
+
 
         BigDecimal rateAmount = calculateConstantRateAmount();
         BigDecimal interestAmount = calculateInterestAmount();
@@ -71,9 +80,14 @@ public class AmountsCalculationServiceImpl implements AmountsCalculationService 
     }
 
 
+    private BigDecimal calculateQ(BigDecimal interestPercent) {
+        return interestPercent.divide(YEAR, 10, RoundingMode.HALF_UP).add(BigDecimal.ONE);
+    }
+
+
     private RateAmounts calculateDecreasingRate(InputData inputData) {
 
-        BigDecimal q = calculateQ(inputData.getInterestPercent());
+
 
         return new RateAmounts();
 
