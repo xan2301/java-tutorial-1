50a743f5506f1bbea3b3e0cb2d5f493da969965a
xan2301 <2silverghost2@gmail.com>
2022-08-04 20:12:28 +0200

    Project day 5.1 21,3944

---
+++ src/com/company/dzien45/jedne/classs.java
@@ -0,0 +1,22 @@
+package com.company.dzien45.jedne;
+
+public class classs {
+
+    public static void main(String[] args) {
+        int x = 7;
+        A(); {
+            x = 8;
+        }
+        {
+            x = 9;
+        }
+
+        System.out.println(x);
+    }
+
+    private static void A() {
+    }
+
+
+
+}
